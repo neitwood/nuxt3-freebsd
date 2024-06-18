e2bf5867cbc2136d9c09b7e733389693a3108f88
Alexey Pilyugin <apilyugin@miatel.ru>
2024-06-18 11:23:05 +0300

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: true,
+  srcDir: 'src',
+  devtools: { enabled: true },
+  app: {
+    buildAssetsDir: '/_nuxt/'
+  }
 })
